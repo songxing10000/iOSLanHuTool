e277f3f42873f7825cc74933d422ed3e53b6e593
songxing10000 <songxing10000@live.cn>
2020-05-17 00:09:27 +0800

    加入可存储的选项配置

--- popup.js
+++ popup.js
@@ -22,3 +22,22 @@ function onWindowLoad() {
 }
 // 窗口载入时使用自己的载入函数
 window.onload = onWindowLoad;
+
+/// 新加面板
+document.addEventListener('DOMContentLoaded', function() {
+	var defaultConfig = {color: 'white', showImage: true}; // 默认配置
+	// 读取数据，第一个参数是指定要读取的key以及设置默认值
+	chrome.storage.sync.get(defaultConfig, function(items) {
+		document.getElementById('color').value = items.color;
+		document.getElementById('show_image').checked = items.showImage;
+	});
+});
+
+document.getElementById('save').addEventListener('click', function() {
+	var color = document.getElementById('color').value;
+	var showImage = document.getElementById('show_image').checked;
+	chrome.storage.sync.set({color: color, showImage: showImage}, function() {
+		document.getElementById('status').textContent = '保存成功！';
+		setTimeout(() => {document.getElementById('status').textContent = '';}, 800);
+	});
+});
