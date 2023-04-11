7eb0059d885f076a9e230a04faacdaf616a480b9
songxing10000 <songxing10000@live.cn>
2023-04-11 17:13:29 +0800

    up

--- getPagesSource.js
+++ getPagesSource.js
@@ -2,8 +2,23 @@
 /// 通过 domcument 拼接相应 字符串
 function DOMtoString(document_root) {
     var loadUrl = document.URL;
-    document.body.contentEditable='true'
-    if (loadUrl.includes('cnblogs.com')) {
+    
+    // csdn可复制
+    document.body.contentEditable = 'true'
+
+    if (loadUrl.includes('jianshu')) {
+
+        function exchange(el1, el2) {
+            var ep1 = el1.parentNode; var ep2 = el2.parentNode;
+            var index1 = Array.prototype.indexOf.call(ep1.children, el1);
+            var index2 = Array.prototype.indexOf.call(ep2.children, el2);
+            ep2.insertBefore(el1, ep2.children[index2]);
+            ep1.insertBefore(el2, ep1.children[index1]);
+        }
+        // 简书 把评论放到推荐的八卦上面来
+        exchange(document.getElementById('note-page-comment'), document.getElementsByClassName('ouvJEz')[1])
+    }
+    else if (loadUrl.includes('cnblogs.com')) {
         moveReleaseDataToTop()
     }
     else if (loadUrl.includes('pgyer.com')) {
@@ -19,7 +34,7 @@ function DOMtoString(document_root) {
 
         // 属性面板
         let propertyDiv = document.getElementsByClassName('annotation_item')[1]
-        if(typeof propertyDiv === 'undefined') {
+        if (typeof propertyDiv === 'undefined') {
             // 没有选中某个元素
             return
         }
@@ -35,20 +50,20 @@ function DOMtoString(document_root) {
             rgbaStrs = rgbas[0].replace('RGBA', '').split(', ')
         } else {
             propertyDiv = document.getElementsByClassName('annotation_item')[2]
-            if(typeof propertyDiv === 'undefined') {
+            if (typeof propertyDiv === 'undefined') {
                 // 选中的是图片，没有RGB
             } else {
                 propertyStrs = propertyDiv.innerText.split('\n')
-           
+
                 rgbas = propertyStrs.filter(str => str.includes('RGBA'))
                 if (rgbas.length > 0) {
                     rgbaStrs = rgbas[0].replace('RGBA', '').split(', ')
                 }
             }
-            
+
         }
-        
-        if(rgbaStrs.length >= 4) {
+
+        if (rgbaStrs.length >= 4) {
             returnObj.r = rgbaStrs[0]
             returnObj.g = rgbaStrs[1]
             returnObj.b = rgbaStrs[2]
@@ -56,22 +71,22 @@ function DOMtoString(document_root) {
         } else {
             // 获取rgba异常
         }
-        
-        
-        
-        
+
+
+
+
         // 默认在iOS平台开发
         let developmentPlatform = 'iOS'
         // 代码面板默认取objc
         let codeDivs = document.getElementsByClassName(' language-c')
 
-        if(codeDivs.length <= 0) {
+        if (codeDivs.length <= 0) {
             // 没有选中某个元素
             return
         }
         // 开发平台 iOS  Android Web 小程序
         let developmentPlatformElements = document.getElementsByClassName('not_define')
-        if (typeof developmentPlatformElements !== 'undefined'  && developmentPlatformElements.length > 0) {
+        if (typeof developmentPlatformElements !== 'undefined' && developmentPlatformElements.length > 0) {
 
             developmentPlatform = developmentPlatformElements[0].innerText
         }
@@ -80,7 +95,7 @@ function DOMtoString(document_root) {
         if (isAndroid) {
             codeDivs = document.getElementsByClassName('language-xml')
         }
-        
+
 
         if (isiOS && (typeof codeDivs === 'undefined' || codeDivs.length <= 0)) {
             // 切图可下载
@@ -92,7 +107,7 @@ function DOMtoString(document_root) {
         }
 
         let codeStr = codeDivs[0].innerText
-        if(codeStr.length <= 0) {
+        if (codeStr.length <= 0) {
             // 没有选中某个元素
             return
         }
@@ -119,13 +134,13 @@ function DOMtoString(document_root) {
                 returnCodeStr = returnCodeStr.replaceAll('PingFang SC-Regular', 'PingFangSC-Regular')
                 returnCodeStr = returnCodeStr.replaceAll('苹方 粗体', 'PingFangSC-Semibold')
                 returnCodeStr = returnCodeStr.replaceAll('苹方 中等', 'PingFangSC-Medium')
- 
 
-                
+
+
                 returnObj.returnCodeStr = returnCodeStr
                 return returnObj
             }
-            let fontName =  propertyStrs[propertyStrs.indexOf('字体')+1]
+            let fontName = propertyStrs[propertyStrs.indexOf('字体') + 1]
             fontName = fontName.replaceAll('苹方-简 细体', 'PingFangSC-Light')
             fontName = fontName.replaceAll('苹方-简 常规体', 'PingFangSC-Regular')
             fontName = fontName.replaceAll('苹方-简 中黑体', 'PingFangSC-Medium')
@@ -144,32 +159,32 @@ function DOMtoString(document_root) {
             } else {
                 returnObj.a = 1
             }
-            
-            returnObj.text = propertyStrs[propertyStrs.indexOf('内容')+1]
+
+            returnObj.text = propertyStrs[propertyStrs.indexOf('内容') + 1]
             returnObj.fontName = fontName
-            returnObj.fontSize = propertyStrs[propertyStrs.indexOf('字号')+1].replace('pt', '')
+            returnObj.fontSize = propertyStrs[propertyStrs.indexOf('字号') + 1].replace('pt', '')
             returnObj.hexColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
             return returnObj
         }
 
         if (codeStr.includes('UIView')) {
 
-            
+
             if (propertyStrs[0] === '颜色') {
                 returnObj.hexColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
                 return returnObj
-            } 
-            else  if (propertyStrs[0] === '中心边框') {
+            }
+            else if (propertyStrs[0] === '中心边框') {
                 /*
                 中心边框,粗细,0.5pt,#000000 10%,HEX,,HEX#000000,,AHEX#1A000000,,HEXA#0000001A,,RGBA0, 0, 0, 0.1,,HSLA0, 0%, 0%, 0.1
                 */
-               if (returnObj.height == 0) {
-                   returnObj.height = propertyStrs[propertyStrs.indexOf('粗细')+1].replace('pt', '')
-                   returnObj.hexColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
+                if (returnObj.height == 0) {
+                    returnObj.height = propertyStrs[propertyStrs.indexOf('粗细') + 1].replace('pt', '')
+                    returnObj.hexColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
 
-               }
+                }
             }
-           
+
             else {
                 if (propertyStrs[0].includes('#') && propertyStrs[0].includes('%')) {
                     returnObj.hexColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
@@ -182,7 +197,7 @@ function DOMtoString(document_root) {
                     propertyStrs[1] === '粗细' &&
                     propertyStrs[2].includes('pt') &&
                     propertyStrs[3].includes('#') && propertyStrs[3].includes('%')) {
-                        
+
                     returnObj.borderWidth = propertyStrs[2].replace('pt', '')
                     returnObj.borderColor = propertyStrs.filter(str => str.includes('#') && str.includes('%'))[0]
                     return returnObj
@@ -205,36 +220,36 @@ function getReturnObj() {
     if (frameDiv.innerText.length <= 0) {
         frameDiv = document.getElementsByClassName('annotation_item')[1]
     }
-    if(typeof frameDiv === 'undefined') {
+    if (typeof frameDiv === 'undefined') {
         // 没有选中某个元素
         return
     }
     let frameStrs = frameDiv.innerText.split('\n')
-    if(frameStrs.length == 1 && frameStrs[0] === '') {
+    if (frameStrs.length == 1 && frameStrs[0] === '') {
         // 没有选中某个元素
         return
     }
     // x,y
     let postIdx = frameStrs.indexOf('位置')
-    let x = frameStrs[postIdx+1].replaceAll('pt', '')
-    let y = frameStrs[postIdx+2].replaceAll('pt', '')
+    let x = frameStrs[postIdx + 1].replaceAll('pt', '')
+    let y = frameStrs[postIdx + 2].replaceAll('pt', '')
     let sizeIdx = frameStrs.indexOf('大小')
     // w,h
-    let width = frameStrs[sizeIdx+1].replaceAll('pt', '')
-    let height = frameStrs[sizeIdx+2].replaceAll('pt', '')
+    let width = frameStrs[sizeIdx + 1].replaceAll('pt', '')
+    let height = frameStrs[sizeIdx + 2].replaceAll('pt', '')
 
     // corner
     let corner = '0'
     let cornerIdx = frameStrs.indexOf('圆角')
     if (cornerIdx >= 0) {
         // 有圆角
-        corner = frameStrs[cornerIdx+1].replaceAll('pt', '')
+        corner = frameStrs[cornerIdx + 1].replaceAll('pt', '')
         if (corner.includes('  ')) {
             // 288,52,87,24,12  0  0  12  
             // 暂不考虑不是四个角圆角的 
             corner = corner.split('  ').filter(str => parseInt(str) > 0)[0]
         }
-    } 
+    }
     // tip 键与值相同，可以简写
     return { x, y, width, height, corner }
 }
